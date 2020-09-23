19fb833c8a45d8e5f5f8c55d276002f68e38ebbc
Vladimir <xssqq38488@gmail.com>
2020-09-23 23:54:11 +0300

    ver 1.1
    homework 2

---
+++ ChatApp/ChatApp/ProfileViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ProfileViewController.swift
+//  ChatApp
+//
+//  Created by Vladimir Konovalenko on 16.09.2020.
+//  Copyright © 2020 Vladimir Konovalenko. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var logoView: UIView!
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?){
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        //print("Edit button frame in init is: \(editButton.frame)")
+        //тут будет runtime error, так как свойства еще не проинициализированны
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        print("Edit button frame in viewDidLoad is: \(editButton.frame)")
+        }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        print("Edit button frame in viewDidAppear is: \(editButton.frame)")
+    }
+    
+    private func setupView() {
+        logoView.layer.cornerRadius = logoView.bounds.width / 2
+        editButton.titleEdgeInsets = UIEdgeInsets(top: 16, left: 0, bottom: 0, right: 0)
+        logoView.backgroundColor = UIColor(hex: "#E4E82BFF")
+        saveButton.backgroundColor = UIColor(hex: "#F6F6F6FF")
+        saveButton.layer.cornerRadius = 14
+    }
+    
+}
+
